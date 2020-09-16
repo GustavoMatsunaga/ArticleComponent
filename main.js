1b80e2060c1d66aa4c8dbb4c4d8d802f0415aa5f
GustavoMatsunaga <63619508+GustavoMatsunaga@users.noreply.github.com>
2020-09-16 12:01:24 -0300

    ADD: Responsive Design

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-const share = document.querySelector("#share-button")
+const screen = document.querySelector(".main")
+const shareIcon = document.getElementById("share-icon")
 const box = document.querySelector("#share-box")
-share.addEventListener('click', () => {
+shareIcon.addEventListener('click', () => {
     box.classList.toggle("hidden")
+    shareIcon.classList.toggle("activated")
+    shareColorChange()
 })
+
+function shareColorChange () {
+    if (screen.clientWidth <= 760) {
+            shareIcon.src = "../images/icon-share.svg";
+            shareIcon.style.backgroundColor = "hsl(210, 46%, 95%)";
+    }
+    else {
+        if (shareIcon.classList.length > 1) {
+            shareIcon.src = "../images/icon-share-inverted.svg";
+            shareIcon.style.backgroundColor = "hsl(217, 19%, 35%)";
+    
+        } else {
+            shareIcon.src = "../images/icon-share.svg";
+            shareIcon.style.backgroundColor = "hsl(210, 46%, 95%)";
+        }
+    }
+}
